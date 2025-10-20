b202be7681503affbf5acdc5c09bedb74281f094
IncognicatJess <jaisyunurm@gmail.com>
2025-10-20 10:23:17 +0700

    contact edit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,27 +1,31 @@
 export default function Contact() {
   const contacts = [
     {
-      name: "Master",
-      phone: "0812-xxxx-xxxx",
-      ig: "@master.dev",
+      name: "Jaisyu Nur Muhammadi",
+      phone: "0851-8475-3922",
+      ig: "@jeizoo",
+      img: "/jes2.png",
       color: "from-[var(--color-blue)] to-[var(--color-green)]",
     },
     {
-      name: "Anggota 2",
+      name: "Kesya Noventa Salsabila",
       phone: "0813-xxxx-xxxx",
       ig: "@anggota2_ui",
+      img: "/anggota2.png",
       color: "from-[var(--color-orange)] to-[var(--color-purple)]",
     },
     {
-      name: "Anggota 3",
+      name: "Evan Wahyu Ezra Manurung",
       phone: "0814-xxxx-xxxx",
       ig: "@anggota3_code",
+      img: "/anggota3.png",
       color: "from-[var(--color-green)] to-[var(--color-blue)]",
     },
     {
-      name: "Anggota 4",
+      name: "Nayla Raissa Putri",
       phone: "0815-xxxx-xxxx",
       ig: "@anggota4_art",
+      img: "/anggota4.png",
       color: "from-[var(--color-purple)] to-[var(--color-orange)]",
     },
   ];
@@ -38,7 +42,7 @@ export default function Contact() {
             className={`p-6 rounded-2xl text-white shadow-lg bg-gradient-to-br ${c.color} hover:scale-105 transition-transform`}
           >
             <img
-              src="/profile-default.png"
+              src={c.img}
               alt={c.name}
               className="w-24 h-24 mx-auto rounded-full border-4 border-white mb-4 object-cover"
             />
